49577056704f8458a59155b43ace58a86e5384a0
Tejpratap Kushwaha <tkushwaha@XEBIAINDIA.LOCAL>
2020-04-30 12:48:17 +0800

    code on the basis of role

--- app-customer-service/src/main/java/com/tej/customer/CustomerServiceApplication.java
+++ app-customer-service/src/main/java/com/tej/customer/CustomerServiceApplication.java
@@ -3,11 +3,13 @@ package com.tej.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.tej.modal.customer")
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
--- app-customer-service/src/main/java/com/tej/customer/controller/ProfileController.java
+++ app-customer-service/src/main/java/com/tej/customer/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package com.tej.customer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import com.tej.customer.service.CustomerService;
@@ -16,16 +17,19 @@ public class ProfileController {
 	CustomerService customerService;
 
 	@RequestMapping(value = "/profile", method = RequestMethod.POST)
+	@PreAuthorize("hasAuthority('create_profile')")
 	public Customer save(@RequestBody Customer customer) {
 		return customerService.save(customer);
 	}
 
 	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+	@PreAuthorize("hasRole('ROLE_MANAGER')")
 	public Customer fetch(@RequestParam int profileId) {
 		return customerService.fetchById(profileId);
 	}
 
 	@RequestMapping(value = "/profiles", method = RequestMethod.GET)
+	@PreAuthorize("hasAnyAuthority('create_profile')")
 	public List<Customer> fetch() {
 		return customerService.fetchAllProfiles();
 	}
